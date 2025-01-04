fd8f7080c8de6f41b609aa1589e392eadc81b3cc
KaungWai <kaungwai219@gmail.com>
2025-01-04 20:25:49 +0900

    feat(module4): add debug folder

---
+++ src/module4/debug/app.js
@@ -0,0 +1,12 @@
+console.log('Start')
+doThings()
+console.log('End')
+
+function doThings() {
+    console.log('Task 1')
+    console.log('Task 2')
+    // debugger
+    console.log('Task 3')
+    console.log('Task 4')
+    console.log('Task 5')
+}
