a1fb338fe891e9847e6873d06a848693b795e91d
MeMeZinHtun <memezintun@gmail.com>
2025-01-05 21:29:48 +0900

    feat(module4): create esm package #18

---
+++ src/module4/esm/meme/index.js
@@ -0,0 +1,9 @@
+import { PI, subtract } from '../../commonjs/meme/lib/math.js'
+import log from './lib/log.js'
+
+import * as math from './lib/math.js'
+
+log.doInfoLog(PI);
+log.doInfoLog(subtract(1,1));
+log.doWarningLog('warning');
+log.doErrorLog('Error');
---
+++ src/module4/esm/meme/lib/log.js
@@ -0,0 +1,13 @@
+export default {
+    doInfoLog: function(text) {
+        console.log(`Info: ${text}`)
+    },
+
+    doWarningLog: function(text) {
+        console.log(`Warning: ${text}`)
+    },
+
+    doErrorLog: function(text) {
+        console.log(`Error: ${text}`)
+    }
+}
