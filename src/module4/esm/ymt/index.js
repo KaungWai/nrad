37ed296dca44d2a11ca330ea135ab447f51b4c87
yinminthumyawin-coding <yinminthu61295@gmail.com>
2025-01-05 21:33:14 +0900

    feat(module4): create esm package #16

---
+++ src/module4/esm/ymt/index.js
@@ -0,0 +1,5 @@
+import log from './lib/log.js'
+import math from './lib/math.js'
+
+log.doInfoLog(math.PI)
+math.doInfoLog(divide(4,2))
---
+++ src/module4/esm/ymt/lib/log.js
@@ -0,0 +1,11 @@
+function doInfoLog(text) {
+    console.log(`Info: ${text}`)
+  }
+
+  function doWarningLog(text) {
+    console.warn(`Warning: ${text}`)
+  }
+
+  function doErrorLog (text) {
+    console.error(`Error: ${text}`)
+  }
