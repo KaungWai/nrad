eb4a6a9c774efa3d80d380cab2f2277b43d14d1b
MeMeZinHtun <memezintun@gmail.com>
2025-01-05 20:49:57 +0900

    feat(module4): create cjs package #14

---
+++ src/module4/commonjs/meme/lib/log.js
@@ -0,0 +1,13 @@
+module.exports = {
+    doInfoLog: function(text){
+        console.log(`Info: ${text}`)
+    },
+
+    doWarningLog: function(text){
+        console.log(`Warning: ${text}`)
+    },
+    
+    doErrorLog: function(text){
+        console.log(`Error: ${text}`)
+    }
+}
