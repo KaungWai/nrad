31c6ad8461c729774488ace2d4aa6cc2881a7772
KaungWai <kaungwai219@gmail.com>
2025-01-12 19:03:09 +0900

    add event folder

---
+++ src/module4/event/kw/index.js
@@ -0,0 +1,26 @@
+const { temperatureSensor } = require('./sensor')
+
+// department 1: alert people if temp over 35
+const listener1 = (temp) => {
+    if(temp > 35) {
+        console.log('Department1: Waring! Warning! Temp is ' + temp)
+    }
+}
+temperatureSensor.on('measure', listener1)
+
+// department 2: evacuate people engine if temp over 40
+const listener2 = (temp) => {
+    if(temp > 40) {
+        console.log('Department2: Everybody go to the emergency exit!')
+        temperatureSensor.removeListener('measure', listener2)
+    }
+}
+temperatureSensor.on('measure', listener2) 
+
+// department 3: prepare gears if temp over 50
+const listener3 = (temp) => {
+    if(temp > 50) {
+        console.log('Department3: Equipments ready!')
+    }
+}
+temperatureSensor.on('measure', listener3)
---
+++ src/module4/event/kw/sensor.js
@@ -0,0 +1,19 @@
+const { EventEmitter } = require('node:events')
+
+// emitter
+const temperatureSensor = new EventEmitter()
+
+setInterval(() => {
+    const temp = Math.random() * 100
+    console.log('Sensor: ', temp)
+    if (temp > 80) {
+        temperatureSensor.emit('error', new Error('Sensor burned'))
+    } else {
+        temperatureSensor.emit('measure', temp)
+    }
+    
+}, 5000)
+
+module.exports = {
+    temperatureSensor
+}
