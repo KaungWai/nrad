a08da8ab41d7177cf2d745304f66a76951abfed0
MeMeZinHtun <memezintun@gmail.com>
2025-02-23 20:09:09 +0900

    feat(module7) Create createUserHandler #43

--- src/module7/collab/handlers/createUserHandler.ts
+++ src/module7/collab/handlers/createUserHandler.ts
@@ -1,7 +1,48 @@
 import { MyRequest, MyResponse } from "../types/types";
-import * as Yup from 'yup'
+import * as yup from 'yup';
 import { prismaInstance } from '../utils/prisma';
 
+const userSchema = yup.object({
+    user_name: yup.string().max(50).required(),
+    user_email: yup.string().max(200).email().required(),
+});
+
+type userType = yup.InferType<typeof userSchema>
+
 export async function createUserHandler(request: MyRequest, response: MyResponse) {
-    //
+    try {
+        await userSchema.validate (request.myBody, {
+            abortEarly: false,
+        });
+        
+        const user: userType = userSchema.cast(request.myBody)       
+ 
+        const createUser = await prismaInstance.user.create({
+            data: {
+                user_name: user.user_name,
+                user_email: user.user_email,
+            }     
+        });
+        response.writeHead( 201, {
+            "Content-Type": "application/json",
+        })
+        response.end(JSON.stringify(createUser));
+    } catch (error) {
+        if(error instanceof yup.ValidationError) {
+            response.writeHead(401, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: error.errors
+            }))
+        } else {
+            console.log(error)
+            response.writeHead(500, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: "Internal Server Error"
+            }))
+        }
+    }
 }
