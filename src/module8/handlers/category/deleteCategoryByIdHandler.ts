67a59f46c207011a53f21e17e768bdf05cb2e762
mtcodingtech <mtcodingtech@gmail.com>
2025-02-25 22:26:12 +0630

    feat(module8): update and delete category

--- src/module8/handlers/category/deleteCategoryByIdHandler.ts
+++ src/module8/handlers/category/deleteCategoryByIdHandler.ts
@@ -1,5 +1,34 @@
 import { MyRequest, MyResponse } from "../../types/types";
+import { prismaInstance } from "../../utils/prisma";
+import { requestUtils } from "../../utils/requestUtils";
 
-export function deleteCategoryByIdHandler(request: MyRequest, response: MyResponse) {
-    // MT
+
+export async function deleteCategoryByIdHandler(request: MyRequest, response: MyResponse) {
+    const urlObj = requestUtils.getURLObject(request)
+    const categoryId = urlObj.pathname.split('/').filter(x => x.trim() !== "")[1]
+
+    const category = await prismaInstance.category.findUnique({
+        where: {
+            category_id: categoryId
+        }
+    })
+
+    if (!category) {
+        response.writeHead(404, {
+            "Content-Type": "application/json",
+        })
+        response.end(JSON.stringify({
+            error: "category not found"
+        }))
+        return
+    }
+
+    await prismaInstance.category.delete({
+        where: {
+            category_id: categoryId
+        }
+    })
+
+    response.writeHead(204)
+    response.end()
 }
