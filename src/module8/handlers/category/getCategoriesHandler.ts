bc86d69d8a9dccf4e97950c0342d8d4c4a1fc875
MeMeZinHtun <memezintun@gmail.com>
2025-03-01 19:37:34 +0900

    feat(module8): Create getCategoryHandler and getCategoryByIdHandler

--- src/module8/handlers/category/getCategoriesHandler.ts
+++ src/module8/handlers/category/getCategoriesHandler.ts
@@ -1,5 +1,59 @@
 import { MyRequest, MyResponse } from "../../types/types";
+import * as Yup from 'yup';
+import { queryUtils } from '../../utils/queryUtil';
+import { requestUtils } from '../../utils/requestUtils';
+import { prismaInstance } from "../../utils/prisma";
 
-export function getCategoriesHandler(request: MyRequest, response: MyResponse) {
-    // MEME
+const querySchema = Yup.object({
+    filter: Yup.object({
+        category_id: Yup.string().min(36).max(36),
+        category_name: Yup.string().max(200),
+    }),
+})
+
+type Query = Yup.InferType<typeof querySchema>
+
+export async function getCategoriesHandler(request: MyRequest, response: MyResponse) {
+
+    const urlObj = requestUtils.getURLObject(request)
+    let query: Query = queryUtils.decodeQuery(urlObj);
+
+    try {
+        await querySchema.validate(query)
+        query = querySchema.cast(query)
+
+        const category = await prismaInstance.category.findMany({
+            where: {
+                category_id: query.filter.category_id,
+                category_name: {
+                    contains: query.filter.category_name,
+                    mode: 'insensitive'
+                },
+            },
+        })
+    
+        response.writeHead(200, {
+            "Content-Type": "application/json",
+        })
+    
+        const responseData = { category: category }
+        response.end(JSON.stringify(responseData))
+    } catch(e) {
+        if(e instanceof Yup.ValidationError) {
+            response.writeHead(400, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: e.errors
+            }))
+        } else {
+            console.log(e)
+            response.writeHead(500, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: "Internal server error"
+            }))
+        }
+    }
 }
--- src/module8/handlers/category/getCategoryByIdHandler.ts
+++ src/module8/handlers/category/getCategoryByIdHandler.ts
@@ -1,5 +1,31 @@
 import { MyRequest, MyResponse } from "../../types/types";
+import * as Yup from 'yup'
+import { queryUtils } from '../../utils/queryUtil';
+import { requestUtils } from '../../utils/requestUtils';
+import { prismaInstance } from '../../utils/prisma';
 
-export function getCategoryByIdHandler(request: MyRequest, response: MyResponse) {
-    // MEME
+export async function getCategoryByIdHandler(request: MyRequest, response: MyResponse) {
+    const urlObj = requestUtils.getURLObject(request)
+    const categoryId = urlObj.pathname.split('/').filter(x => x.trim() !== "")[1]
+
+    const category = await prismaInstance.category.findUnique({
+        where: {
+            category_id: categoryId
+        }
+    })
+
+    if (!category) {
+        response.writeHead(404, {
+            "Content-Type": "application/json",
+        })
+        response.end(JSON.stringify({
+            error: "category not found"
+        }))
+        return
+    }
+
+    response.writeHead(200, {
+        "Content-Type": "application/json",
+    })
+    response.end(JSON.stringify(category))
 }
