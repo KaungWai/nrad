d25b0fb3852cba909dbdc6e8aa28deeb47947970
mtcodingtech <mtcodingtech@gmail.com>
2025-03-02 11:20:07 +0630

    feat(module8): crud publishers

--- src/module8/handlers/publisher/deletePublisherByIdHandler.ts
+++ src/module8/handlers/publisher/deletePublisherByIdHandler.ts
@@ -1,4 +1,34 @@
 import { MyRequest, MyResponse } from "../../types/types";
+import { prismaInstance } from "../../utils/prisma";
+import { requestUtils } from "../../utils/requestUtils";
+
 
 export async function deletePublisherByIdHandler(request: MyRequest, response: MyResponse) {
+    const urlObj = requestUtils.getURLObject(request)
+    const publisherId = urlObj.pathname.split('/').filter(x => x.trim() !== "")[1]
+
+    const publisher = await prismaInstance.publisher.findUnique({
+        where: {
+            publisher_id: publisherId
+        }
+    })
+
+    if (!publisher) {
+        response.writeHead(404, {
+            "Content-Type": "application/json",
+        })
+        response.end(JSON.stringify({
+            error: "publisher not found"
+        }))
+        return
+    }
+
+    await prismaInstance.publisher.delete({
+        where: {
+            publisher_id: publisherId
+        }
+    })
+
+    response.writeHead(204)
+    response.end()
 }
--- src/module8/handlers/publisher/updatePublisherByIdHandler.ts
+++ src/module8/handlers/publisher/updatePublisherByIdHandler.ts
@@ -1,4 +1,70 @@
 import { MyRequest, MyResponse } from "../../types/types";
+import * as Yup from 'yup'
+import { requestUtils } from "../../utils/requestUtils";
+import { prismaInstance } from "../../utils/prisma";
+
+const bodySchema = Yup.object({
+    publisher_name: Yup.string().max(200).required(),
+})
+
+type Body = Yup.InferType<typeof bodySchema>
 
 export async function updatePublisherByIdHandler(request: MyRequest, response: MyResponse) {
+    try {
+        const urlObj = requestUtils.getURLObject(request)
+        const publisherId = urlObj.pathname.split('/').filter(x => x.trim() !== "")[1]
+
+        const targetPublisher = await prismaInstance.publisher.findUnique({
+            where: {
+                publisher_id: publisherId
+            }
+        })
+
+        if(!targetPublisher) {
+            response.writeHead(404, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: 'Publisher not found'
+            }))
+            return
+        }
+
+        await bodySchema.validate(request.myBody, {
+            abortEarly: false
+        })
+        const body = bodySchema.cast(request.myBody)
+
+        const upatedUser = await prismaInstance.publisher.update({
+            where: {
+                publisher_id: publisherId
+            },
+            data: {
+                publisher_name: body.publisher_name,
+                updated_at: new Date()
+            }
+        })
+
+        response.writeHead(201, {
+            "Content-Type": "application/json",
+        })
+        response.end(JSON.stringify(upatedUser))
+    } catch (e) {
+        if (e instanceof Yup.ValidationError) {
+            response.writeHead(400, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: e.errors
+            }))
+        } else {
+            console.log(e)
+            response.writeHead(500, {
+                "Content-Type": "application/json",
+            })
+            response.end(JSON.stringify({
+                error: "Internal server error"
+            }))
+        }
+    }
 }
