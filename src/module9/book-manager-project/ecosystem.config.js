53bfc74c8ca92e89c8ff6df6d86080e0c9795209
KaungWai <kaungwai219@gmail.com>
2025-04-05 20:40:53 +0900

    feat(module9): update router & ecosystem.config.js

--- src/module9/book-manager-project/ecosystem.config.js
+++ src/module9/book-manager-project/ecosystem.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  apps: [
-    {
-      name: 'book-manager-api',
-      script: 'git pull origin main && npm install && npx prisma migrate deploy && npx prisma generate && npx tsc && node ./dist/index.js',
-    },
-  ],
-}
+  apps : [{
+    name: 'book-manager-api',
+    script: './dist/index.js',
+  }],
+};
